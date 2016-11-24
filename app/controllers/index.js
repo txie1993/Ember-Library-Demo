ad0fb0a9d6741cbc02551c22b4011828fbe5aca1
Tony Xie <tx248@nyu.edu>
2016-11-24 13:48:48 -0800

    first commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    isDisabled: true
+
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,22 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyDbnh0gKTMlEeYWvjEcXOjYxyjHxhFo1Gs",
+      authDomain: "library-test-e5a52.firebaseapp.com",
+      databaseURL: "https://library-test-e5a52.firebaseio.com",
+      storageBucket: "library-test-e5a52.appspot.com",
+      messagingSenderId: "874163036038"
+   },
+
+   // if using ember-cli-content-security-policy
+   contentSecurityPolicy: {
+     'script-src': "'self' 'unsafe-eval' apis.google.com",
+     'frame-src': "'self' https://*.firebaseapp.com",
+     'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+   },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
